9a556603b9bdca94646abd9df41f74048ec0bb6d
aadilk96 <aadilanilkumar@gmail.com>
2019-11-09 21:13:15 +0100

    yikes

--- jHack19/flyeco/views.py
+++ jHack19/flyeco/views.py
@@ -2,20 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-# posts = [
-#     {
-#         'author': 'aadilk',
-#         'title' : 'Blog Post 1', 
-#         'content': 'First',
-#         'date_posted': 'Nov 9, 2019'
-#     },
-#     {
-#         'author': 'test',
-#         'title' : 'Blog Post 2', 
-#         'content': 'Second',
-#         'date_posted': 'Nov 9, 2019'
-#     }
-# ]
 
 def home(request): 
     context = {
@@ -24,4 +10,4 @@ def home(request):
     return render(request, 'flyeco/home.html', context)
 
 def about(request): 
-    return render(request, 'flyeco/about.html', {'title':'About'}) 
+    return render(request, 'flyeco/about.html', {'title':'About'})
