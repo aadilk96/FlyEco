844ef68c63a98131197d0cb094e19f1a05a2b533
aadilk96 <aadilanilkumar@gmail.com>
2019-11-09 14:03:00 +0100

    projectSetUp

---
+++ jHack19/jHack19/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jHack19 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jHack19.settings')
+
+application = get_wsgi_application()
